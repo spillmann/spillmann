1fb32a3a3766dcdacdfc38f4f30b45ad66858f42
spillmann <mail@martin-spillmann.de>
2015-01-15 10:11:26 +0100

    Erster Test

---
+++ Lambdaexpression/src/de/martin/spillmann/Start.java
@@ -0,0 +1,24 @@
+package de.martin.spillmann;
+
+import java.util.Arrays;
+import java.util.function.IntUnaryOperator;
+
+public class Start {
+	
+	public static void main (String [] args) {
+		Start start = new Start();
+		start.done();
+	}
+
+	
+	private void done() {
+
+//		int factor = 1000;
+//		IntUnaryOperator times1000 = (int x) -> {return x * factor;};
+//		Arrays.stream(new int[]{1,2,3,4,5}).map(times1000).forEach(y->System.out.println(y+1));
+
+		int factor = 1000;
+		IntUnaryOperator times1000 = (int x) -> {return x * factor;};
+		Arrays.stream(new int[]{1,2,3,4,5}).map(times1000).forEach(System.out::println);
+	}
+}
